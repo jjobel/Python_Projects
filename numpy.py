d8497b63f4ced92732d3b31ee24a0229e847e708
jjobel <43651622+jjobel@users.noreply.github.com>
2018-10-09 11:26:21 -0700

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import numpy as np 
+ 
+x = 1.0
+y = 2.0 
+
+print(np.sin(x))
+print(np.cos(x))
+print(np.tan(x))
+print(np.arcsin(x))
+print(np.arccos(x))
+print(np.arctan(x))
+print(np.arctan2(x,y))
+print(np.rad2deg(x))
+
+print(np.sinh(x))
+print(np.cosh(x))
+print(np.tanh(x))
+print(np.tanh(x))
+print(np.arctanh(x))
---
+++ useful_modules.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3 
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+import sys 
+import os 
+
+print(sys.argv)
+print(os.getcwd())
+
