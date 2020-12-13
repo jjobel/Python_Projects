da0b6eb99ad0defb41f67db66ee728821a16c7ad
Jose Eias Jobel <jjobel.elias@gmail.com>
2020-12-12 17:21:28 -0800

    Moved files

---
+++ Python_Scripts/data-types.py
@@ -0,0 +1,21 @@
+#1/usr/bin/env python3
+
+import numpy as np 
+
+i = 10 
+print(type(i))
+
+a_i = np.zeros(i,dtype=int) 
+print(type(a_i))
+print(type(a_i[0]))
+
+x = 119.0
+print(type(x))
+
+y = 1.19e2
+print(type(y))
+
+z = np.zeros(i,dtype=float)
+print(type(z))
+print(type(z[0]))
+
